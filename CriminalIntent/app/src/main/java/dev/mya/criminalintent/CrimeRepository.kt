2b18b6590fa6c418e8c0f05c197de887afd27052
Murat Yalçın ALBAŞ <muratalbas@outlook.com>
2024-06-25 15:04:15 +0300

    chore: Made various improvements

--- CriminalIntent/app/src/main/java/dev/mya/criminalintent/CrimeRepository.kt
+++ CriminalIntent/app/src/main/java/dev/mya/criminalintent/CrimeRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import dev.mya.criminalintent.database.CrimeDatabase
+import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 private const val DATABASE_NAME = "crime-database"
@@ -17,9 +18,10 @@ class CrimeRepository private constructor(context: Context) {
             CrimeDatabase::class.java,
             DATABASE_NAME
         )
+            .createFromAsset(DATABASE_NAME)
             .build()
 
-    suspend fun getCrimes(): List<Crime> = database.crimeDao().getCrimes()
+    fun getCrimes(): Flow<List<Crime>> = database.crimeDao().getCrimes()
 
     suspend fun getCrime(id: UUID): Crime = database.crimeDao().getCrime(id)
 
--- CriminalIntent/app/src/main/java/dev/mya/criminalintent/database/CrimeDao.kt
+++ CriminalIntent/app/src/main/java/dev/mya/criminalintent/database/CrimeDao.kt
@@ -3,13 +3,14 @@ package dev.mya.criminalintent.database
 import androidx.room.Dao
 import androidx.room.Query
 import dev.mya.criminalintent.Crime
+import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
 @Dao
 interface CrimeDao {
 
     @Query("SELECT * FROM crime")
-    suspend fun getCrimes() : List<Crime>
+    fun getCrimes() : Flow<List<Crime>>
 
     @Query("SELECT * FROM crime WHERE id=(:id)")
     suspend fun getCrime(id: UUID) : Crime
