a3aa442357006f85552a25cdce52287fb8701f1b
Murat Yalçın ALBAŞ <muratalbas@outlook.com>
2024-07-05 10:05:16 +0300

    Add suspect selection functionality to CrimeDetailFragment
    - Updated CrimeDetailFragment to allow selecting a suspect from contacts using the ContactsContract.
    - Added a button to CrimeDetailFragment to trigger contact selection.
    - Updated the UI to display the selected suspect's name.
    - Enhanced CrimeRepository and Crime entity to manage and store suspect information.
    - Included necessary permissions and queries in the AndroidManifest.xml to handle contact selection.

--- CriminalIntent/app/src/main/java/dev/mya/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/dev/mya/criminalintent/CrimeDetailFragment.kt
@@ -1,11 +1,17 @@
 package dev.mya.criminalintent
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.provider.ContactsContract.Contacts
 import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
@@ -38,6 +44,12 @@ class CrimeDetailFragment : Fragment() {
         CrimeDetailViewModelFactory(args.crimeID)
     }
 
+    private val selectSupect = registerForActivityResult(
+        ActivityResultContracts.PickContact()
+    ) { uri: Uri? ->
+        uri?.let { parseContactSelection(it) }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -62,6 +74,16 @@ class CrimeDetailFragment : Fragment() {
                     oldCrime.copy(isSolved = isChecked)
                 }
             }
+
+            crimeSuspect.setOnClickListener {
+                selectSupect.launch(null)
+            }
+
+            val selectSupectIntent = selectSupect.contract.createIntent(
+                requireContext(),
+                null
+            )
+            crimeSuspect.isEnabled = canResolveIntent(selectSupectIntent)
         }
 
         viewLifecycleOwner.lifecycleScope.launch {
@@ -109,7 +131,12 @@ class CrimeDetailFragment : Fragment() {
                     Intent.createChooser(reportIntent, getString(R.string.send_report))
                 startActivity(chooserIntent)
             }
+
+            crimeSuspect.text = crime.suspect.ifEmpty {
+                getString(R.string.crime_suspect_text)
+            }
         }
+
     }
 
     private fun getCrimeReport(crime: Crime): String {
@@ -127,5 +154,29 @@ class CrimeDetailFragment : Fragment() {
         }
 
         return getString(R.string.crime_report, crime.title, dateString, solvedString, suspectText)
+    }
+
+    private fun parseContactSelection(contactUri: Uri) {
+        val queryFields = arrayOf(ContactsContract.Contacts.DISPLAY_NAME)
+        val queryCursor = requireActivity().contentResolver
+            .query(contactUri, queryFields, null, null, null)
+
+        queryCursor?.use { cursor ->
+            if (cursor.moveToFirst()) {
+                val suspect = cursor.getString(0)
+                crimeDetailViewModel.updateCrime { oldCrime ->
+                    oldCrime.copy(suspect = suspect)
+                }
+            }
+        }
+    }
+
+    private fun canResolveIntent(intent: Intent): Boolean {
+        val packageManager: PackageManager = requireActivity().packageManager
+        val resolvedActivity: ResolveInfo? = packageManager.resolveActivity(
+            intent,
+            PackageManager.MATCH_DEFAULT_ONLY
+        )
+        return resolvedActivity != null
     }
 }
